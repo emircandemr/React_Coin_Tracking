835a4d56620e268f76b717151c6fd441b6d0362e
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-02-23 23:46:08 +0300

    ✨ addded : coin tracking comp

--- src/components/CoinTracking.jsx
+++ src/components/CoinTracking.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const CoinTracking = () => {
   return (
     <div className='w-full h-24 bg-[#121212]'>
-        <h1>Coin Tracking</h1>
+        <div>
+            
+        </div>
     </div>
   )
 }
