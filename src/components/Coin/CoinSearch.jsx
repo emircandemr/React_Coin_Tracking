b9b3a3b53eacf2a709b67872b72ffac6f9106bdc
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-02-26 23:43:42 +0300

    ✨ refactoring

--- src/components/Coin/CoinSearch.jsx
+++ src/components/Coin/CoinSearch.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CoinSearch = () => {
   return (
-    <div className='w-full h-screen bg-red-500'>
+    <div className='w-full h-screen bg-red-500 absol'>
 
     </div>
   )
