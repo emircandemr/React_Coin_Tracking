259f212c81f26258dbc26b85d8d06c9a1f72a313
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-02-27 23:46:32 +0300

    ✨ added : Auto refresh every 5 minutes

--- src/components/Coin/CoinList.jsx
+++ src/components/Coin/CoinList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCoins } from '../../store/Slice/coin-slice'
 import {IoMdArrowRoundBack,IoMdArrowRoundForward} from 'react-icons/io'
@@ -6,6 +6,7 @@ import CoinItem from './CoinItem'
 
 const CoinList = () => {
 
+    const ref = useRef(null)
     const dispatch = useDispatch()
     const {coinList} = useSelector(state => state.coinTracking)
     const [currentPage, setCurrentPage] = useState(1)
@@ -29,7 +30,17 @@ const CoinList = () => {
             dispatch(getCoins(currentPage))
     }, [currentPage, dispatch])
 
-
+    // Auto refresh every 5 minutes
+    useEffect(() => {
+        ref.current = setInterval( () => {
+            dispatch(getCoins(currentPage))
+        }, 5 * 60 * 1000)
+        return () => {
+            if (ref.current) {
+                clearInterval(ref.current)
+            }
+        }
+    },[])
 
   return (
     <div className='lg:w-[60%] md:w-[70%] w-full md:h-[calc(100vh_-_130px)] h-1/2 border-r border-gray-300 border-opacity-20 mt-4 flex flex-col justify-center items-center gap-y-5 '>
